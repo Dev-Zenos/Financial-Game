c25f4b028be5a38096597a6f3e0c8aacbadaab95
Dev-Zenos <158857144+Dev-Zenos@users.noreply.github.com>
2024-11-04 11:52:27 -0800

    First commit

---
+++ scripts/Button2.gd
@@ -0,0 +1,26 @@
+extends Button
+
+@onready var sprite = $"../../Label2"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_mouse_entered():
+	sprite.scale *= 1.25;
+	sprite.position.y += 25;
+
+
+func _on_mouse_exited():
+	sprite.scale /= 1.25;
+	sprite.position.y -= 25;
+
+
+	
+	
---
+++ scripts/CheckServer.gd
@@ -0,0 +1,53 @@
+extends HTTPRequest
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	request_completed.connect(_on_request_completed)
+	request("https://financial-game-backend.onrender.com/start_connection")
+	
+
+func _get_user_data():
+	var userId = "none"
+	if FileAccess.file_exists("localStorage.txt"):
+		var save_file = FileAccess.open("localStorage.txt", FileAccess.READ)
+		var json_string = ""
+		json_string += save_file.get_line();
+		userId = json_string
+	request("https://financial-game-backend.onrender.com/fetch_userData?userId=" + userId)
+
+func _get_QBank():
+	request("https://financial-game-backend.onrender.com/fetch_QBank")
+	
+
+func _await_save_user_data(obj):
+	var save_file = FileAccess.open("localStorage.txt", FileAccess.WRITE)
+	save_file.store_line((obj["UniqueID"]+"\n")+(JSON.stringify(obj)))
+
+func _await_save_QBank(obj):
+	var save_file = FileAccess.open("QBank.txt", FileAccess.WRITE)
+	save_file.store_line(JSON.stringify(obj))
+	print("Saving")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _on_request_completed(result, response_code, headers, body):
+	var text = body.get_string_from_utf8()
+	var json = JSON.parse_string(text)
+	var data = JSON.parse_string(json)  # Parse the string again to get the actual object
+	$"../Loading Spinner".get_node("Label").text = data["msg"]
+	if(data["startCode"] == 0):
+		_get_user_data()
+	elif(data["startCode"] == 1):
+		if(data["error"] == true):
+			_await_save_user_data(data)
+			$"../Loading Spinner".get_node("Label").text = "Fetching Question Bank"
+		_get_QBank()
+	elif(data["startCode"] == 2):
+		_await_save_QBank(data)
+		get_tree().change_scene_to_file("res://scenes/start_page.tscn");
+		
+
