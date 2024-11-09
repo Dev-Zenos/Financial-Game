6fdcfa52475a3d495e62425bd48c778d31263bbc
Dev-Zenos <158857144+Dev-Zenos@users.noreply.github.com>
2024-11-08 16:03:41 -0800

    movement

---
+++ scripts/player1.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -250.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravityOn = false
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor() and gravityOn:
+		velocity.y += gravity * delta
+		#print("Applying Gravity")
+
+	# Handle jump.
+	if Input.is_action_just_pressed("move_up") and is_on_floor() and gravityOn:
+		velocity.y = JUMP_VELOCITY
+		
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction < 0:
+		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.play("run")
+	elif direction > 0:
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if(!gravityOn):
+		direction = Input.get_axis("move_down", "move_up") * -1;
+		if direction:
+			velocity.y = direction * SPEED
+			#animated_sprite_2d.play("run")
+			#print("Run")
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	
+
+	move_and_slide()
