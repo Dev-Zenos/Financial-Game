3ed8fa5fd1123f2ec9a5dc2972faa3f0518e1f11
Dev-Zenos <158857144+Dev-Zenos@users.noreply.github.com>
2024-11-26 09:49:33 -0800

    timer

--- scripts/QuestionsPage.gd
+++ scripts/QuestionsPage.gd
@@ -9,4 +9,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var time = $Timer.time_left
-	print(time)
+	time = int(time * 10)
+	var floatTimer = time/10.0;
+	if(floatTimer == int(floatTimer)):
+		floatTimer = str(floatTimer) + ".0"
+	if((str(floatTimer) + "s") != $Timer/Label.text):
+		$Timer/Label.text = (str(floatTimer) + "s")
